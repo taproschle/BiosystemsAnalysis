e7b9ed0c47084f5b94d509220b344e4dca89d934
Alejandro Sepúlveda Celis <aisepulveda2@uc.cl>
2021-04-16 13:49:11 -0400

    pack volumen

--- Modelo/Xu/fedbatch.m
+++ Modelo/Xu/fedbatch.m
@@ -1,26 +1,11 @@
-function dydt = fedbatch(t,x,u)
+function dydt = fedbatch(t,x)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-global Yas Yoa Yos Yxa Yxsof Yxsox Ka KiS Ks Ko qAcmax qOmax qSmax Osat kla mumax
+global Yax YS_ox_X YS_of_X Yoa K_A C_X C_A C_S qm qO_max qAc_max qS_max mu_set Ysa Yso K_i_A K_S Vin Xin Sfeed
 dydt = [];
-mu_set = u(1);
-Xin = u(2);
-Vin = u(3);
-Sin = u(4);
 
-% parametros
-
-
-
-%%
-Ca = 1/30; % mol Carbon / g acetato
-Cs = 1/30; % mol Carbon /g azucar
-Cx = 0.04; % mol Carbon / g biomasa
-
-
-%%
 
 
 % variables de estado
@@ -28,43 +13,30 @@ S = x(1); % sustrato [ g/L]
 %disp(S)
 A = x(2); % acetato [g/L]
 X = x(3); % biomasa [g/L]
-V = x(4); % volumen hasta ahora son 100ml parece [L]
-O = x(5); %g/L
-% ecs
-% oxidative pathway; % glucose uptake [g g-1 celulas h-1]
-% relacion rutas metabolicas
-qS_T = qSmax*S / ((Ks + S)*(1 + A/KiS));
-qO_crit = qOmax*O / (Ko + O);
-qS_ox = min (qS_T , qO_crit /Yos ) ; 
-qS_of   = max (0 , qS_T - qS_ox) ; 
-qAc = min (qAcmax * A / (A + Ka) , (qO_crit - qS_ox*Yos) / Yoa) ;
-% ecs constitutivas 
-qS = qS_ox + qS_of;
-qA = qS_of*Yas - qAc; % qA produccion - qA de consumo
-mu = qS_ox*Yxsox + qS_of*Yxsof + qAc*Yxa; 
-% if mu >= mumax
-%     mu = mumax;
-% end
-% disp(mu)
-qO2 = qS_ox*Yos + qAc*Yoa; % lo q se consume en la via oxidativa + lo que se consume en la via overflow
+V = x(4); % volumen hasta ah
+%ec constitutivas
 
-%qO = qOs + qAc*Yoa;
-
-%mu = (qS_ox - qm)*Yxsox + qS_of*Yxsof + qAc*Yxa;
-
+qS = (qS_max*S/(K_S + S))*1/(1+(A/K_i_A));
+qOs = min((qS-(qS-qm)*YS_ox_X*C_X/C_S)*Yso , qO_max);
+qSox = ((qOs/Yso)-(qm*YS_ox_X*C_X/C_S))/(1-YS_ox_X*C_X/C_S);
+qSof = qS - qSox;
+qAp = (qSof - qSof*YS_of_X*C_X/C_S)*Ysa;
+qAc = min(qAc_max*A/(A+K_A) , (qO_max - qOs)*Yoa/(1-Yax*C_X/C_A));
+mu = (qSox - qm)*YS_ox_X + qSof*YS_of_X + qAc*Yax;
+qO = qOs + (qAc - qAc*Yax*C_X/C_A)*Yoa;
     
-F = mu_set*Vin*Xin*exp(mu_set*t)/(Yxsox*Sin);
+F = mu_set*Vin*Xin*exp(mu_set*t)/(YS_ox_X*Sfeed);
 
 
-dydt(1) =  (F/V)*(Sin-S)- qS*X ; %sustrato
+
+dydt(1) =  (F/V)*(Sfeed-S)- qS*X ; %sustrato
 %if t>=12 && t<=12.5
     %dydt(1) = -qS*X + 10;
 %end
-dydt(2) = (qA)*X - (F/V)*A; % acetato
+dydt(2) = (qAp-qAc)*X - (F/V)*A; % acetato
 %dydt(2) = (mu)*X;
 dydt(3) = (mu)*X - (F/V)*X; % biomass
 dydt(4) = F; % volumen
-dydt(5) = -(qO2)*X + kla*(Osat-O);
 dydt = dydt';
 
 end
--- Modelo/Xu/primerasfiguras.m
+++ Modelo/Xu/primerasfiguras.m
@@ -1,71 +1,52 @@
 clc,clear
 close all
-tspan = [0 20];
-x0 = [13.86 0 0.1 0.1 0.035]; % S A X V O
-[T,C] = ode15s(@batchmodel, tspan, x0);
 
+global  Yax YS_ox_X YS_of_X Yoa K_A C_X C_A C_S qm qO_max qAc_max qS_max mu_set Ysa Yso K_i_A K_S Xin Vin Sfeed
 
-%% yields
-Yas = 0.23; %  g A /g S stochiometric cte CARCAMO
-%Yas = 0.667; % XU
-Yoa = 20.66/1000; % g o2 / g A CARCAMO
-%Yoa = 1.067; % XU 
+Yax = 0.667;
+YS_ox_X = 0.51;
+YS_of_X = 0.15;
+Yoa = 1.067;
+Ysa = 0.667; 
+Yso = 1.067;
+C_X = 0.04;
+C_A = 1/30;
+C_S = 1/30;
+K_A = 0.05;
+K_i_A = 5;
+K_S = 0.05;
+qm = 0.04;
+qO_max = 13.4*32/1000; % g/ g h
+qAc_max = 0.2;
+qS_max = 1.25;
 
-Yos = 401/1000; % g o2 / g S CARCAMO
-%Yos = 1.067; % XU
-%Yxa = 14.55; % g DCW / gIP CARCAMO
-Yxa = 0.4; % XU
-Yxsof = 0.7; %CARCAMO
-%Yxsof = 0.15; % XU
-Yxsox = 0.3; % CARCAMO
-%Yxsox = 0.51; %XU
+mu_set = 0.3;
 
+Xin = 0.4;
+Vin = 6.8;
+Sfeed = 550;
+tspan = [0 24];
+x0 = [0.5 0 Xin Vin]; % S A X V O
+[T,C] = ode15s(@fedbatch, tspan, x0);
 
-%%
-Ca = 1/30; % mol Carbon / g acetato
-Cs = 1/30; % mol Carbon /g azucar
-Cx = 0.04; % mol Carbon / g biomasa
+%
 
- %%
- %%
-%KiO = 4; % g/L fitting
-Ka = 0.5236; % CARCAMO
-%Ka = 0.05; %XU
-KiS = 0.45; % g A /L fitting CARCAMO
-%KiS = 5; % XU
-Ks = 8; % g S/L CARCAMO
-%Ks = 0.05; % XU
-Ko = 0.0045; %g O/L CARCAMO
-%Ko = 1/100;
-qAcmax = 6; % g/g hr CARCAMO
-%qAcmax = 0.06; % XU
-qm = 0.04; % g/ g hr
-qOmax = 83.5/1000; % g/g hr CARCAMO
-%qOmax = 15.6*32/1000; % XU
-qSmax = 4.9; % g/g hr CARCAMO
-%qSmax = 1.3; % Xu
-muc = 0.55;
-Osat = 0.035;
-kla = 180; % [h-1]
+qS = (qS_max.*C(:,1)./(K_S + C(:,1))).*1./(1+(C(:,2)./K_i_A));
+qOs = min((qS-(qS-qm).*YS_ox_X.*C_X./C_S).*Yso , qO_max);
+qSox = ((qOs./Yso)-(qm.*YS_ox_X.*C_X./C_S))./(1-YS_ox_X.*C_X./C_S);
+qSof = qS - qSox;
+qAp = (qSof - qSof.*YS_of_X.*C_X./C_S).*Ysa;
+qAc = min(qAc_max.*C(:,2)./(C(:,2)+K_A) , (qO_max - qOs).*Yoa./(1-Yax.*C_X./C_A));
+mu = (qSox - qm).*YS_ox_X + qSof.*YS_of_X + qAc.*Yax;
+
 
 % oxidative pathway; % glucose uptake [g g-1 celulas h-1]
 % relacion rutas metabolicas
-qS_T = qSmax.*C(:,1)./((Ks + C(:,1)).*(1 + C(:,2)./KiS));
-qO_crit = qOmax.*C(:,5)./(Ko + C(:,5));
-qS_ox = min (qS_T , qO_crit ./Yos) ; 
-qS_of   = max(0,qS_T - qS_ox); 
-qAc = min(qAcmax.*C(:,2)./(C(:,2) + Ka),(qO_crit - qS_ox.*Yos)./Yoa) ;
-% ecs constitutivas 
-qS = qS_ox + qS_of;
-qA = qS_of.*Yas - qAc; % qA produccion - qA de consumo
-mu = qS_ox.*Yxsox + qS_of.*Yxsof + qAc.*Yxa; 
-qO2 = qS_ox.*Yos + qAc.*Yoa; % lo q se consume en la via oxidativa + lo que se consume en la via overflow
-%mu = (qS_ox-qm).*Yxsox + qS_of.*Yxsof + qAc.*Yxa; 
-OCR = (qO2).*C(:,3);
+
 
 %%
 figure(1)
-subplot(3,3,1)
+subplot(2,2,1)
 yyaxis left
 plot(T,C(:,1),'g',"LineWidth",2);
 ylim([0 15]);
@@ -74,42 +55,24 @@ yyaxis right
 plot(T,C(:,3),'r',"LineWidth",2);
 ylabel('Biomass [g/L]');
 legend('Glucose','Biomass');
-ylim([0 10]);
 xlabel('Time [h]')
 
-subplot(3,3,2)
+subplot(2,2,2)
 plot(T,C(:,2),'k','LineWidth',2);
 legend('Acetate')
 xlabel('Time [h]')
 ylabel('Acetate [g/L]');
 
-subplot(3,3,3)
-plot(T,mu,'k','LineWidth',2)
-legend('mu')
-ylabel('mu [h-1]')
+subplot(2,2,3)
+plot(T,C(:,4),'g','LineWidth',2);
+legend('Volumen [L]')
 xlabel('Time [h]')
+ylabel('Volume [L]');
 
-subplot(3,3,4)
-plot(T,qAc,'m','LineWidth',2)
-legend('qAc')
-ylabel('qAc [g g-1 h-1]')
+subplot(2,2,4)
+plot(T,mu,'g','LineWidth',2)
+legend('mu [h-1]')
 xlabel('Time [h]')
+ylabel('Specific Growth [h-1]')
 
-subplot(3,3,5)
-plot(T,qO2,'k','LineWidth',2)
-legend('qO')
-xlabel('Time [h]')
-ylabel('qO [gO2 g-1 DW h-1]')
 
-subplot(3,3,6)
-plot(T,OCR,'g','LineWidth',2)
-legend('OCR')
-ylabel(' OCR [gO2 L-1 h-1]')
-xlabel( 'Time [h]')
-
-subplot(3,3,7)
-plot(T,C(:,5),'g','LineWidth',2)
-legend('Oxygen [g/L]')
-xlabel(' Time [h]')
-ylabel(' Oxygen [g/L]')
-clear
