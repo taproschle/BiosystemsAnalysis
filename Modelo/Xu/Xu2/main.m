a09c1260e63182923497cff0aedefeb036fd2e4b
dnavalos <dnavalos@uc.cl>
2021-05-25 21:36:14 -0400

    xu2

--- Modelo/Xu/Xu2/main.m
+++ Modelo/Xu/Xu2/main.m
@@ -1,8 +1,8 @@
+% Main Xu 
 clc ; clear
 close all
 
-O_sat = 850/1000;
-klao2 = 180;
+% Parámetros ajustables:
 Yax = 0.667;
 YS_ox_X = 0.51;
 YS_of_X = 0.15;
@@ -15,29 +15,35 @@ C_S = 1/30;
 K_A = 0.05;
 K_i_A = 5;
 K_S = 0.05;
-K_O =  0.0001; % g o2 L-1
 qm = 0.04;
 qO_max = 13.4*32/1000; % g/ g h
 qAc_max = 0.2;
 qS_max = 1.25;
 
-mu_set = 0.43;
-
-Xin = 0.4;
+% Parámetros no ajustables:
+mu_set = 0.29;
+klao2 = 180*10;
 Vin = 6.8;
+Xin = 0.4;
 Sfeed = 550;
+O_sat = 0.03; %850/1000;
+K_O =  0.0001; % g o2 L-1
 
+% Lista de parámetros ajustables entregados al modelo
 k = [YS_ox_X YS_of_X Yax Yoa Ysa Yso C_X C_A C_S qm ...
     qS_max qAc_max qO_max K_S K_i_A K_A];
+
+% Lista de parámetros no ajustables:
 v = [mu_set klao2 Vin Xin Sfeed O_sat K_O];
+
+% Resolución ODE:
 
 tspan = [0 25];
 x0 = [0.5 0 Xin Vin 2/1000]; % S A X V O
-
 fun = @(t,y) xu_model(t,y,v,k);
-
 [T,C] = ode23s(fun, tspan, x0);
 
+% Gráfico:
 figure(1)
 subplot(2,2,1)
 yyaxis left
